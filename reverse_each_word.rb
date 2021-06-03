2856635290b7c939dbdf8d744a3c586780126c95
Jonathan Yu <jonathan.yu.jwy2@yale.edu>
2021-06-03 04:23:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
   arr = string.split
-  new_arr = []
-  arr.each do |word|
-    new_arr << word.reverse
+  arr.map do |word|
+    word.reverse
   end
-  new_arr.join(" ")
+  arr.join(" ")
 end
